0e1b196dbd3ff5924c61b1e5f36833030ba88587
Sakari Mursu <sakari@mursu.dev>
2020-07-11 14:20:46 +0300

    Move example bot from own repo to here

---
+++ example/src/constants/env.ts
@@ -0,0 +1,1 @@
+export const BOT_AUTH_TOKEN: string = process.env.BOT_AUTH_TOKEN ?? '';
---
+++ example/src/index.ts
@@ -0,0 +1,4 @@
+import { Monbot } from 'monbot';
+import { BOT_AUTH_TOKEN } from './constants/env';
+
+Monbot({ authToken: BOT_AUTH_TOKEN });
