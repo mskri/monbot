512125f363b62f9cfc99ee84d06008dc8cdd991f
Sakari Mursu <sakari@mursu.dev>
2020-08-07 20:52:01 +0300

    Refactor command param types

--- src/command.ts
+++ src/command.ts
@@ -1,22 +1,18 @@
 import { Message, Snowflake } from 'discord.js';
 import type { ParsedArgs, ParseArgsOptions } from './parseArgs';
 
+type CommandExtraParams = {
+  content: string;
+  parseArgs: <T extends Record<string, string | number | boolean | Date>>(
+    input: string,
+    opts?: ParseArgsOptions
+  ) => ParsedArgs<T>;
+};
+
 export type Command = {
   name: string;
   trigger: RegExp;
-  run: (
-    message: Message,
-    {
-      content,
-      parseArgs,
-    }: {
-      content: string;
-      parseArgs: <T extends Record<string, string | number | boolean | Date>>(
-        input: string,
-        opts?: ParseArgsOptions
-      ) => ParsedArgs<T>;
-    }
-  ) => void;
+  run: (message: Message, { content, parseArgs }: CommandExtraParams) => void;
   requiredRoles?: Snowflake[];
   adminOnly?: boolean;
   channels?: string[];
