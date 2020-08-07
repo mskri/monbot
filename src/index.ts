ce1a1c0fb0f956ac89f5f054c0951f871afbbac1
Sakari Mursu <sakari@mursu.dev>
2020-08-07 22:11:50 +0300

    Remove exporting discord Message

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Monbot } from './bot';
 import { createCommand } from './command';
-import { Message } from 'discord.js';
 
-export { Monbot, createCommand, Message };
+export { Monbot, createCommand };
