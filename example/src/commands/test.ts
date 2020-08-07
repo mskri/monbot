7f5d85ad47ce89256eab4c12c3c8f8edee0a37e4
Sakari Mursu <sakari@mursu.dev>
2020-08-07 20:04:07 +0300

    Remove test command

--- example/src/commands/test.ts
+++
@@ -1,9 +0,0 @@
-import { createCommand, Message } from 'monbot';
-
-export const test = createCommand({
-  name: 'test',
-  trigger: /^!test/,
-  run: function (message: Message) {
-    message.reply('It works!');
-  },
-});
