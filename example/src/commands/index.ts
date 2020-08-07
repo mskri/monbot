c6a1c93c02c02ccdf9cc90442d8af9d2737dd977
Sakari Mursu <sakari@mursu.dev>
2020-08-07 20:17:16 +0300

    f remove test

--- example/src/commands/index.ts
+++ example/src/commands/index.ts
@@ -1,8 +1,7 @@
-import { test } from './test';
 import { hello } from './hello';
 import { channelId } from './channelId';
 import { emojiId } from './emojiId';
 import { roleId } from './roleId';
 import { adminOnly } from './adminOnly';
 
-export const commands = [hello, test, channelId, emojiId, roleId, adminOnly];
+export const commands = [hello, channelId, emojiId, roleId, adminOnly];
