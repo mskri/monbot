71dd283c1ecef07d0ef577b6856dc64c9dfc4944
Sakari Mursu <sakari@mursu.dev>
2020-08-07 21:37:44 +0300

    Add parseArgs test

---
+++ src/parseArgs.spec.ts
@@ -0,0 +1,63 @@
+import { parseArgs } from './parseArgs';
+
+describe('parseArgs', () => {
+  it('parses string into object', () => {
+    expect(
+      parseArgs(
+        '--first --second "is a string" --third false --fourth "2020-02-01 12:34:55+02:00" --fifth 123'
+      )
+    ).toEqual({
+      args: {
+        _: [
+          '--first',
+          '--second',
+          'is a string',
+          '--third',
+          'false',
+          '--fourth',
+          '2020-02-01 12:34:55+02:00',
+          '--fifth',
+          123,
+        ],
+        first: true,
+        second: 'is a string',
+        third: false,
+        fourth: new Date('2020-02-01 12:34:55+02:00'),
+        fifth: 123,
+      },
+      hasMissingArgs: false,
+      missingArgs: [],
+    });
+  });
+
+  it('parses string into object with default values', () => {
+    expect(
+      parseArgs('--title "This is title"', {
+        defaults: { description: 'This is default description' },
+      })
+    ).toEqual({
+      args: {
+        _: ['--title', 'This is title'],
+        title: 'This is title',
+        description: 'This is default description',
+      },
+      hasMissingArgs: false,
+      missingArgs: [],
+    });
+  });
+
+  it('parses string into object and returns missing required args', () => {
+    expect(
+      parseArgs('--title "This is title"', {
+        requiredArgs: ['description'],
+      })
+    ).toEqual({
+      args: {
+        _: ['--title', 'This is title'],
+        title: 'This is title',
+      },
+      hasMissingArgs: true,
+      missingArgs: ['description'],
+    });
+  });
+});
