52106248a943e1da139fade6e77c36eec25995b8
Sakari Mursu <sakari@mursu.dev>
2020-08-09 21:33:39 +0300

    Throw error from translations if not enough params
    If the number of given params does not match to the number of placeholders found in translation show error. Prevents incorrect translations.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,8 @@
 import en from './locales/en.json';
 
-export const t = (key: string, ...parameters: (string | number | null | undefined)[]): string => {
+const placeholderRegExp = /\${[\w._]+}/g;
+
+export const t = (key: string, ...params: (string | number | null | undefined)[]): string => {
   const locale = en as Record<string, string>;
   const translation = locale[key] as string;
 
@@ -8,11 +10,19 @@ export const t = (key: string, ...parameters: (string | number | null | undefine
     throw new ReferenceError(`Could not find translation for '${key}'`);
   }
 
-  if (parameters.length > 0) {
+  const placeholders: RegExpMatchArray = translation.match(placeholderRegExp) ?? [];
+
+  if (placeholders.length !== params.length) {
+    throw new Error(
+      `Number of given params does not match with number of placeholders in translation. Expected to receive ${placeholders.length}, got ${params.length}`
+    );
+  }
+
+  if (params.length > 0) {
     let index = 0;
 
-    return translation.replace(/\${[\w._]+}/g, () => {
-      const filledTranslation = parameters[index] as string;
+    return translation.replace(placeholderRegExp, () => {
+      const filledTranslation = params[index] as string;
       index++;
       return filledTranslation;
     });
